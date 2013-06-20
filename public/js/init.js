ccb2da7c44e2abe2393da78148b34cc9ad1b6608
caasi Huang <caasi.igd@gmail.com>
2013-06-21 06:06:19 +0800

    add simple blogspot support

--- public/js/init.js
+++ public/js/init.js
@@ -45,67 +45,71 @@ App.Link = Ember.Object.extend({
   dest: null
 });
 
-$.post("/story/", function(data) {
-  var story, stories, num, i, para, range, tail, len;
+$.post(
+  "/story/",
+  { url: "http://blogger.godfat.org/2013/06/blog-post.html" },
+  function(data) {
+    var story, stories, num, i, para, range, tail, len;
 
-  story = App.Story.create({
-    title: data.title
-  });
+    story = App.Story.create({
+      title: data.title
+    });
 
-  Array.forEach(data.contents, function(p) {
-    story.contents.pushObject(
-      App.Paragraph.create({
-        text: p
-      })
-    );
-  });
+    Array.forEach(data.contents, function(p) {
+      story.contents.pushObject(
+        App.Paragraph.create({
+          text: p
+        })
+      );
+    });
 
-  App.set("storyRoot", story);
+    App.set("storyRoot", story);
 
-  num = 3;
-  tail = 0;
+    num = 3;
+    tail = 0;
 
-  for (i = 0; i < num; ++i) {
-    para = Math.floor(Math.random() * story.contents.length);
-    len = story.contents[para].text.length;
-    range = {
-      from: tail + Math.floor(Math.random() * (len - tail)),
-      to: (tail + 1) + Math.floor(Math.random() * (len - tail - 1))
-    };
-    if (range.from > range.to) {
+    for (i = 0; i < num; ++i) {
+      para = Math.floor(Math.random() * story.contents.length);
+      len = story.contents[para].text.length;
       range = {
-        from: range.to,
-        to: range.from
+        from: tail + Math.floor(Math.random() * (len - tail)),
+        to: (tail + 1) + Math.floor(Math.random() * (len - tail - 1))
+      };
+      if (range.from > range.to) {
+        range = {
+          from: range.to,
+          to: range.from
+        }
       }
+      tail = range.to;
+
+      (function(para, range) {
+        $.post("/story/", function(data) {
+          var story, link, i;
+
+          story = App.Story.create({
+            position: App.getPosition(),
+            title: data.title
+          });
+
+          Array.forEach(data.contents, function(p) {
+            story.contents.push(
+              App.Paragraph.create({
+                text: p
+              })
+            );
+          });
+
+          link = App.Link.create({
+            range: range,
+            dest: story
+          });
+
+          App.storyRoot.contents.get(para).links.pushObject(link);
+        });
+      }(para, range));
     }
-    tail = range.to;
 
-    (function(para, range) {
-      $.post("/story/", function(data) {
-        var story, link, i;
-
-        story = App.Story.create({
-          position: App.getPosition(),
-          title: data.title
-        });
-
-        Array.forEach(data.contents, function(p) {
-          story.contents.push(
-            App.Paragraph.create({
-              text: p
-            })
-          );
-        });
-
-        link = App.Link.create({
-          range: range,
-          dest: story
-        });
-
-        App.storyRoot.contents.get(para).links.pushObject(link);
-      });
-    }(para, range));
+    App.advanceReadiness();
   }
-
-  App.advanceReadiness();
-});
+);
--- src/server.ls
+++ src/server.ls
@@ -1,13 +1,52 @@
 require! request
 require! express
+require! jquery
 
-moretext = (options, cb) ->
-  request.get(
-    \http://more.handlino.com/sentences.json?n= + options.n + \&limit= + options.limit
-    (err, res, body) ->
-      if not err and res.statusCode is 200
-        cb JSON.parse body
-  )
+$ = do jquery.create
+
+Story =
+  demoURL: \http://blogger.godfat.org/2013/06/blog-post.html
+  getArticle: (url, cb) ->
+    request.get(
+      url
+      (err, res, body) ->
+        if not err and res.statusCode is 200
+          cb body
+    )
+  source:
+    moretext: (cb) ->
+      title <- Story.util.moretext do
+        n: 1
+        limit: 10
+      contents <- Story.util.moretext do
+        n: 3
+        limit: 150
+      cb do
+        title: title.sentences[0]
+        contents: contents.sentences
+    blogspot: (body, cb) ->
+      result = []
+      $body = $ body
+      $title = $body.find \.post-title
+      $contents = $body.find ".post-body p"
+      $contents.each (index, element) ->
+        paragraphs = $(element).html()
+        paragraphs = paragraphs.replace /(<br \/>)\s*(<br \/>)+\s*/g \\u2029
+        paragraphs = paragraphs.replace /(<br \/>)\s*/g \\n
+        paragraphs = paragraphs.split \\u2029
+        Array.prototype.push.apply result, paragraphs
+        if index is $contents.length - 1
+          cb do
+            title: $title.text().trim()
+            contents: result
+  util:
+    moretext: (options, cb) ->
+      request.get(
+        \http://more.handlino.com/sentences.json?n= + options.n + \&limit= + options.limit
+        (err, res, body) ->
+          if not err and res.statusCode is 200
+            cb JSON.parse body
+      )
 
 app = express!
 app
@@ -15,18 +54,13 @@ app
   .use express.methodOverride!
   .use express.static __dirname + \/public
   .post \/story, (req, res) ->
-    ret =
-      title: void
-      contents: void
-    body <- moretext do
-      n: 1
-      limit: 10
-    ret.title = body.sentences[0]
-    body <- moretext do
-      n: 3
-      limit: 150
-    ret.contents = body.sentences
-    res.json ret
+    if req.body.url is void
+      story <- Story.source.moretext
+      res.json story
+    else
+      body <- Story.getArticle req.body.url
+      story <- Story.source.blogspot body
+      res.json story
   .listen 8080
 
 console.log \ready
